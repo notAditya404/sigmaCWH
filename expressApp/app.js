c4abdfb0a3fb63624a6aaed757836692fd6170da
Aditya Yadav <yaditya2025@gmail.com>
2025-09-04 14:34:34 +0530

    POST req handler using cors and json parser

--- expressApp/app.js
+++ expressApp/app.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
+  console.log("RECEIVED GET req at /")
   res.render("index")
+})
+
+app.post('/', express.json() , (req, res) => {
+  console.log("RECEIVED POST req at /")
+  console.log(req.body)
+  res.send("submitted")
 })
 
 app.listen(port, () => {
